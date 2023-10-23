4ae1fcfce08b9c83e1c98dc613ca42e138c827e9
zehracodel <145125942+zehracodel@users.noreply.github.com>
2023-10-23 14:36:43 +0300

    Add files via upload

---
+++ Bolun2-Uygulama32/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bolun2_Uygulama32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int sayi1, sayinin_karesi;
+            try
+            {
+                sayi1 = Convert.ToInt16(textBox1.Text);
+                sayinin_karesi = sayi1 * sayi1;
+                MessageBox.Show(sayinin_karesi.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Hatalı giriş yaptınız!");
+            }
+            finally
+            {
+            }
+        }
+    }
+}
